77cb7108e1f2cb874ac7ec6db81bb423ec1920d2
Seiji Sogabe <s.sogabe@gmail.com>
2015-09-09 21:56:19 +0900

    fixed @deprecated method

--- src/main/java/org/jenkinsci/plugins/gitbucket/GitBucketProjectProperty.java
+++ src/main/java/org/jenkinsci/plugins/gitbucket/GitBucketProjectProperty.java
@@ -31,20 +31,21 @@ import hudson.model.Action;
 import hudson.model.Job;
 import hudson.model.JobProperty;
 import hudson.model.JobPropertyDescriptor;
+import hudson.model.Run;
 import java.util.Collection;
 import java.util.Collections;
 import org.kohsuke.stapler.DataBoundConstructor;
 
 /**
  * Project property
- * 
+ *
  * @author sogabe
  */
 public class GitBucketProjectProperty extends JobProperty<AbstractProject<?, ?>> {
 
-    private String url;
-    
-    private boolean linkEnabled;
+    private final String url;
+
+    private final boolean linkEnabled;
 
     public String getUrl() {
         return url;
@@ -59,7 +60,7 @@ public class GitBucketProjectProperty extends JobProperty<AbstractProject<?, ?>>
         this.url = GitBucketUtil.trimEndSlash(url);
         this.linkEnabled = linkEnabled;
     }
-    
+
     @Override
     public Collection<? extends Action> getJobActions(AbstractProject<?, ?> job) {
         if (url == null) {
@@ -67,8 +68,8 @@ public class GitBucketProjectProperty extends JobProperty<AbstractProject<?, ?>>
         }
         return Collections.singletonList(new GitBucketLinkAction(this));
     }
-    
-    public static GitBucketProjectProperty get(AbstractBuild<?, ?> build) {
+
+    public static GitBucketProjectProperty get(Run<?, ?> build) {
         if (build == null) {
             return null;
         }
@@ -76,9 +77,14 @@ public class GitBucketProjectProperty extends JobProperty<AbstractProject<?, ?>>
         if (build instanceof MatrixRun) {
             job = ((MatrixRun) build).getProject().getParent();
         } else {
-            job = build.getProject();
+            job = build.getParent();
         }
         return job.getProperty(GitBucketProjectProperty.class);
+    }
+
+    @Deprecated
+    public static GitBucketProjectProperty get(AbstractBuild<?, ?> build) {
+        return get((Run<?, ?>) build);
     }
 
     @Extension
